6f6c16ac2f4bac1ad8e6b5d1e9ec9b1d0c352b6c
cheripai <datdo@rocketmail.com>
2015-04-28 13:33:19 -0700

    Schema now has init and repr functions

--- schema.py
+++ schema.py
@@ -4,7 +4,7 @@ from FIXME import db
 
 class Restaurant(db.model):
     
-    __tablename__ = "restaurant"
+    __tablename__ = "restaurants"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
@@ -13,10 +13,18 @@ class Restaurant(db.model):
     dishes = db.relationship('Dish', backref='restaurant')
     # should there be location? and how will we reference multiple locations
 
+    def __init__(self, name, category, image):
+        self.name = name
+        self.category = category
+        self.image = image
+
+    def __repr__(self):
+        return '<Restaurant {}'.format(self.name)
+
 
 class Dish(db.model):
     
-    __tablename__ = "dish"
+    __tablename__ = "dishes"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
@@ -33,3 +41,25 @@ class Dish(db.model):
     soy = db.Column(db.Boolean, nullable=True)
     wheat = db.Column(db.Boolean, nullable=True)
     notes = db.Column(db.String, nullable=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
+    def __init__(self, name, date, price, image, dairy, egg, fish, gluten, 
+                 meat, nut, shellfish, soy, wheat, notes, restaurant_id):
+        self.name = name
+        self.date = date
+        self.price = price
+        self.image = image
+        self.dairy = dairy
+        self.egg = egg
+        self.fish = fish
+        self.gluten = gluten
+        self.meat = meat
+        self.nut = nut
+        self.shellfish = shellfish
+        self.soy = soy
+        self.wheat = wheat
+        self.notes = notes
+        self.restaurant_id = restaurant_id
+
+    def __repr__(self):
+        return '<Dish {}>'.format(self.name)
