18f04badd4f2e60fadd14eb5844d61d592042537
cheripai <datdo@rocketmail.com>
2015-04-28 14:24:44 -0700

    remove junk

--- _config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-DATABASE = 'data.db'
-DATABASE_PATH = os.path.join(basedir, DATABASE)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
