36789521e10083ba729f43366035c071339c8315
mimic <asahibeeru@gmail.com>
2015-05-20 17:05:43 -0700

    function updates for User constructors

--- db_create.py
+++ db_create.py
@@ -21,19 +21,19 @@ def db_create():
     db.configure_mappers()
     db.create_all()
 
-       
+    '''      
     for i in range(50):
         if i % 4 == 0:
             entry = Restaurant('test {}'.format(i), 'vegetarian', 'img')
         elif i % 4 == 1:
             entry = Restaurant('test {}'.format(i), 'vegan', 'img')
         elif i % 4 == 2:
-            entry = User('test {}'.format(i), '0{}'.format(i), 'img')
-            entry.name = 'email{}@email.org'.format(i)
+            entry = User('test {}'.format(i), '0{}'.format(i), 'img', 'email{}@email.org'.format(i))
         else:
            entry = Dish('testdish{}'.format(i), 0.00, '', None, None, None, None, None, 
                      None, None, None, None, None, None, None, '', None, None) 
         db.session.add(entry)
+    '''
     
 
     db.session.commit()
