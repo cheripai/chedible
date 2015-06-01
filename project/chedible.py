680381736963915f09131903e8c67fb0337b9168
cheripai <datdo@rocketmail.com>
2015-06-01 15:06:17 -0700

    Add 404 page

--- project/chedible.py
+++ project/chedible.py
@@ -53,6 +53,11 @@ def load_user():
         g.user = None
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
 # Creates decorator to restrict routes to logged in users
 def login_required(test):
     @wraps(test)
