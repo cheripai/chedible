b6b2748e5fdb9771e39a5b86afb6c98597686b23
cheripai <datdo@rocketmail.com>
2015-07-06 10:53:44 -0700

    Change carousel to tabbed carousel

--- project/static/js/script.js
+++ project/static/js/script.js
@@ -60,3 +60,29 @@ $("[id^=downvote]").click(function(){
         count.text(data.result);
     });
 });
+
+
+/* Animates carousel on index page */
+$(document).ready( function() {
+    $('#indexCarousel').carousel({
+		interval:   4000
+	});
+	
+	var clickEvent = false;
+	$('#indexCarousel').on('click', '.nav a', function() {
+			clickEvent = true;
+			$('.nav li').removeClass('active');
+			$(this).parent().addClass('active');		
+	}).on('slide.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.nav').children().length - 1;
+			var current = $('.nav li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.nav li').first().addClass('active');	
+			}
+		}
+		clickEvent = false;
+	});
+});
