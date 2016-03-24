d9e6cf5cceeb42f3f2ef435f408b0dbe3be4e522
cheripai <datdo@rocketmail.com>
2016-03-24 09:11:43 -0700

    Remove unnecessary code

--- db_create.py
+++ db_create.py
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
 from project import db
 from project.schema import *
 
@@ -11,23 +10,6 @@ def db_create():
     db.drop_all()
     db.configure_mappers()
     db.create_all()
-
-    '''
-    for i in range(50):
-        if i % 4 == 0:
-            entry = Restaurant('test {}'.format(i), 'vegetarian', '', '', None)
-        elif i % 4 == 1:
-            entry = Restaurant('test {}'.format(i), 'vegan', None, '', None)
-        elif i % 4 == 2:
-            entry = User('test {}'.format(i), '0{}'.format(i), None,
-                         'email{}@email.org'.format(i))
-        else:
-            entry = Dish('testdish{}'.format(i), '0.00', '', None, True, None,
-                         False, False, True, False, True, None, None, None,
-                         None, 1, 1)
-        db.session.add(entry)
-    '''
-
     db.session.commit()
 
 
