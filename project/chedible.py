273a6da29e662e824204756db7bc4341c3b370b1
cheripai <datdo@rocketmail.com>
2016-06-22 19:18:24 -0700

    Redirect after uploading photo for restaurant

--- project/chedible.py
+++ project/chedible.py
@@ -363,6 +363,7 @@ def add_restaurant_photo(restaurant_id):
                 flash('File uploaded')
         else:
             flash('Invalid file')
+        return redirect(url_for('add_restaurant_photo', restaurant_id=restaurant_id))
     return render_template('upload.html', type='restaurant', form=form, restaurant_id=restaurant_id)
 
 
